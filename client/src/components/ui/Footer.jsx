344cac94ffcc06e99eaaf70bc9131bf22e41f0aa
Yash Gupta <yash.gupta_ug23@nsut.ac.in>
2025-08-07 00:04:27 +0530

    minor changes

--- client/src/components/ui/Footer.jsx
+++ client/src/components/ui/Footer.jsx
@@ -10,11 +10,9 @@ const Footer = () => {
       <Container>
         <div className="d-flex gap-1 justify-content-center align-items-center flex-column mb-5 py-sm-4">
           <span>
-            <a href="https://github.com/danutama" className="text-secondary fs-3" target="_blank" rel="noopener noreferrer">
               <FaGithub />
-            </a>
           </span>
-          <small className="text-secondary text-center">&copy;{currentYear} Designed and built by Danu Pratama</small>
+          <small className="text-secondary text-center">&copy;{currentYear} Designed and built by Mohit Yadav</small>
           <small className="text-secondary text-center">
             <span>Data provided by </span>
             <a href="https://www.themoviedb.org/" className="text-secondary fw-normal" target="_blank" rel="noopener noreferrer">
